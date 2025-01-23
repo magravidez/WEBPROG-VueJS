4a2ae47b5bc9ba18d7a6e62ea4498c6e53eeb169
Mariel <marielagravidez@gmail.com>
2025-01-23 16:22:07 +0800

    fooditem.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <h1>Hello, world! This is Mariel's website.</h1>
   <p>MEOW MEOW MEOW MEOW MEOW MEOW MEOW</p>
+
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <script></script>
-<style></style>
+
+<style>
+  #app > div {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style>     
