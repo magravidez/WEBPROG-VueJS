d500fe62cf435a8e4835919bf2de43ed1dfe00b1
Mariel <marielagravidez@gmail.com>
2025-01-29 18:12:03 +0800

    supabase 3

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -16,6 +16,22 @@
 
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
 <style>
 
 .container {
--- vue-project/src/components/Countries.vue
+++ vue-project/src/components/Countries.vue
@@ -4,18 +4,4 @@
     </ul>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from '../lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
+<script></script>
