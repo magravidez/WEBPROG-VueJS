792bdb7e48defb81b74d5689b140f98b7a6a6b50
Mariel <marielagravidez@gmail.com>
2025-01-29 18:07:40 +0800

    supabase 2

--- vue-project/src/components/Countries.vue
+++ vue-project/src/components/Countries.vue
@@ -6,7 +6,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 const countries = ref([])
 
