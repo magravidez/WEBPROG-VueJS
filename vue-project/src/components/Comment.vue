5c2229dbccd41c1d44a83698e0c5da6686f3f3bc
Mariel <marielagravidez@gmail.com>
2025-02-05 12:49:40 +0800

    sana okay na

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -5,18 +5,6 @@
     </ul>
   </template>
   
-  <script></script>
-  
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
-
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from '../lib/supabaseClient'
